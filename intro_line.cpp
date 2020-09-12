af888b379adb94f6aea703d7a2877e9f7da55d47
Yoshikiyo <mantohihi785@g.ecc.u-tokyo.ac.jp>
2020-09-12 09:50:16 +0900

    lineの概念を追加した

--- intro_line.cpp
+++ intro_line.cpp
@@ -1,6 +1,8 @@
 #include <ncurses.h>
 #include <iostream>
 #include <string>
+#include <cassert>
+#include <vector>
 using namespace std;
 
 void moveforward(int y, int x, int ymax, int xmax);
@@ -14,11 +16,62 @@ class Line {
     int ch_num;
 };
 
-Line* init_lines(int);
-void show_lines(Line*, WINDOW*);
-
 void init_mycolors() {
   init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+}
+
+void complement(vector<int> &line_alloc) {
+//  assert(line_alloc[0] != -1);
+  for (int i = 1; i < line_alloc.size(); i++) {
+    if (line_alloc[i] == -1) {
+      line_alloc[i] = line_alloc[i-1];
+    }
+  }
+}
+
+void init_alloc(vector<int> &line_alloc) {
+  for (int i=0; i<line_alloc.size(); i++) {
+    line_alloc[i] = -1;
+  }
+}
+
+Line* init_lines(int n) {
+  Line* head = new Line;
+  head->content = "hello";
+  Line* prev = new Line;
+  head->next = prev;
+  prev->prev = head;
+  prev->content = "hello--------------------------------------------------------------------------";
+  for(int i = 0; i < n-1; i++) {
+    Line* now = new Line;
+    now->content = "hello";
+    prev->next = now;
+    now->prev = prev;
+    now->next = NULL;
+    prev = now;
+  }
+  return head;
+}
+
+void show_lines(Line* head, WINDOW* win, vector<int> &line_alloc) {
+  init_alloc(line_alloc);
+  Line* now = head;
+  int x, y;
+  int i = 1;
+  move(0, 0);
+  while(now->next != NULL) {
+    getyx(win, y, x);
+    line_alloc[y] = i;
+    attrset(COLOR_PAIR(1));
+    mvprintw(y, 0, "%2d", i);
+    attrset(0);
+    mvaddstr(y, 3, now->content.c_str());
+    getyx(win, y, x);
+    move(y+1, 4);
+    now = now->next;
+    i++;
+  }
+  complement(line_alloc);
 }
 
 int main() {
@@ -31,10 +84,21 @@ int main() {
   start_color();
   init_mycolors();
   getmaxyx(win, ymax, xmax);
+  vector<int> line_alloc(ymax);
   WINDOW* subwindow = subwin(win, 3, xmax, ymax-3, 0);
   waddstr(subwindow, "This is subwindow");
   Line* lines = init_lines(20);
-  show_lines(lines, win);
+  try {
+  show_lines(lines, win, line_alloc);
+  } catch(const char* error) {
+    endwin();
+    cout << error << endl;
+    return EXIT_FAILURE;
+  }
+  wmove(subwindow, 2, 0);
+  for (int i=0; i<ymax; i++) {
+    wprintw(subwindow, "%d ", line_alloc[i]);
+  }
   while(true) {
     getyx(win, y, x);
     wmove(subwindow, 1, 0);
@@ -79,38 +143,4 @@ void movebackward(int y, int x, int ymax, int xmax) {
   }
 }
 
-Line* init_lines(int n) {
-  Line* head = new Line;
-  head->content = "hello";
-  Line* prev = new Line;
-  head->next = prev;
-  prev->prev = head;
-  prev->content = "hello---------------------------------------------";
-  for(int i = 0; i < n-1; i++) {
-    Line* now = new Line;
-    now->content = "hello";
-    prev->next = now;
-    now->prev = prev;
-    now->next = NULL;
-    prev = now;
-  }
-  return head;
-}
 
-void show_lines(Line* head, WINDOW* win) {
-  Line* now = head;
-  int x, y;
-  int i = 1;
-  move(0, 0);
-  while(now->next != NULL) {
-    getyx(win, y, x);
-    attrset(COLOR_PAIR(1));
-    mvprintw(y, 0, "%2d", i);
-    attrset(0);
-    mvaddstr(y, 3, now->content.c_str());
-    getyx(win, y, x);
-    move(y+1, 4);
-    now = now->next;
-    i++;
-  }
-}
